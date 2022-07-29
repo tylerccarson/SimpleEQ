035aaf727c872032c2ea2286569920212faede48
Tyler <tyler.c.carson@gmail.com>
2022-07-29 08:56:58 -0700

    Customized bypass button look and feel

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -173,6 +173,11 @@ struct LookAndFeel : juce::LookAndFeel_V4
                            float rotaryStartAngle,
                            float rotaryEndAngle,
                            juce::Slider&) override;
+    
+    void drawToggleButton (juce::Graphics &g,
+                           juce::ToggleButton &toggleButton,
+                           bool shouldDrawButtonAsHighlighted,
+                           bool shouldDrawButtonAsDown) override;
 };
 
 struct RotarySliderWithLabels : juce::Slider
@@ -313,6 +318,8 @@ private:
     analyzerEnabledButtonAttachment;
     
     std::vector<juce::Component*> getComps();
+    
+    LookAndFeel lnf;
     
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SimpleEQAudioProcessorEditor)
 };
